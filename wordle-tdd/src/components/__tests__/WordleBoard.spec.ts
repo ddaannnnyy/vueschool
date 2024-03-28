e1a27f18358dc17d640d39d8f4b82eb71952391d
ddaannnnyy <32764014+ddaannnnyy@users.noreply.github.com>
2024-03-28 11:56:20 +1000

    use vitest globals

--- wordle-tdd/src/components/__tests__/WordleBoard.spec.ts
+++ wordle-tdd/src/components/__tests__/WordleBoard.spec.ts
@@ -1,9 +1,7 @@
-import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
-describe('HelloWorld', () => {
+describe('WorldBoard', () => {
   it('renders properly', () => {
     const wrapper = mount(WordleBoard, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
