c2ddc82ea983e28d3d0c5d3281fba16912205486
ddaannnnyy <32764014+ddaannnnyy@users.noreply.github.com>
2024-03-28 12:21:43 +1000

    only render feedback after game has ended

--- wordle-tdd/src/components/WordleBoard.vue
+++ wordle-tdd/src/components/WordleBoard.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings'
+import { ref } from 'vue'
+
 defineProps<{
   wordOfTheDay: string
 }>()
+
+const guessInProgress = ref('')
+const guessSubmitted = ref('')
 </script>
 
 <template>
   <p>{{ wordOfTheDay }}</p>
-  <input type="text" name="" id="" />
-  <p>{{ VICTORY_MESSAGE }}</p>
-  <p>{{ DEFEAT_MESSAGE }}</p>
+  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress" />
+  <p
+    v-if="guessSubmitted.length > 0"
+    v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+  ></p>
 </template>
